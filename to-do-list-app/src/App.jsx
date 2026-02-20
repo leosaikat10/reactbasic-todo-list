f760f1daaf11e243850830968d40a3f46569f01e
Saikat Biswas <saikatbiswas24dev@gmail.com>
2026-02-20 19:20:34 +0530

    States are added

--- to-do-list-app/src/App.jsx
+++ to-do-list-app/src/App.jsx
@@ -2,13 +2,36 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
+  // State to hold the current task input
+  const [task, setTask] = useState("");
+
+  //State for task list
+  const [tasks, setTasks] = useState([]);
+
+  // Function to handle adding a new task
+  const addTask = () => {
+    if (task.trim() !== "") 
+      setTasks([...tasks, task]);
+      setTask(""); // Clear the input field after adding the task
+    };
+
+    // delete task handler
+    const deleteTask = (index) => {
+      const newTasks = tasks.filter((_, i) => i !== index);
+      setTasks(newTasks);
+    };
+
   return (
     <div className="App">
         <div className="app-container">
           <div className="heading"><h1>To-Do List App</h1>
           <p>Manage your tasks efficiently</p>
           </div>
-          <input id="task-input" className="task-input" type="text" placeholder="Add a new task..." /><button>Add Task</button>
+          <div className="input-section">
+           <input id="task-input" className="task-input" type="text" placeholder="Add a new task..."  value={task} onChange={(e)=> setTask(e.target.value)}/>
+           <button>Add Task</button>
+            
+          </div>
         </div>
     </div>
   )
